cd94f2b7b6f49a52b3be36326db9e633f500e832
vissuaws <vissuaws@gmail.com>
2025-06-29 19:09:03 +1000

    comp

--- .platform/hooks/prebuild/composer.sh
+++ .platform/hooks/prebuild/composer.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-echo "Running composer install..."
-/usr/bin/composer install 
+
+# Check if composer is available
+if ! command -v composer &> /dev/null; then
+  echo "Installing Composer..."
+  curl -sS https://getcomposer.org/installer | php
+  mv composer.phar /usr/local/bin/composer
+fi
+
+# Run Composer install
+composer install --no-dev
+ 
 
 
